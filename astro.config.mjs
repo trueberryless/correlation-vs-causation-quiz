d7baaf2fa220ceec00e079dae8f7523b44de5131
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2025-11-10 20:24:49 +0100

    feat: add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
+  adapter: netlify(),
 });
